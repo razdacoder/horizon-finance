8404749ce968d046a2f39278e4acfd23638ee710
Ramon Rasheed <ramonrash2@gmail.com>
2024-05-13 15:07:03 +0100

    Summary Endpoint

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { eachDayOfInterval, isSameDay } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -18,4 +19,43 @@ export function formatCurrency(value: number) {
     style: "currency",
     currency: "NGN",
   });
+}
+
+export function calculatePercentageChange(current: number, previous: number) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100;
+  }
+
+  return ((current - previous) / previous) * 100;
+}
+
+export function fillMissingDays(
+  activeDays: { date: Date; income: number; expense: number }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return [];
+  }
+
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  });
+
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day));
+
+    if (found) {
+      return found;
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expense: 0,
+      };
+    }
+  });
+
+  return transactionsByDay;
 }
